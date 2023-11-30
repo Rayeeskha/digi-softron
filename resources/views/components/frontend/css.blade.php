1f7b2af88ea0160959d6d20fd1c23a02774f2bf5
khanrayees <rayeesinfotech@gmail.com>
2023-11-30 16:50:38 +0530

    add google console

--- resources/views/components/frontend/css.blade.php
+++ resources/views/components/frontend/css.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta name="google-site-verification" content="Crl2X9iBRE3OSL9_2joRrKll1pwuVTE8_qsaZuGM-N0" />
 
 	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
